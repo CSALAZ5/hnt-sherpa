25c2aa2afb7d8338783c74cd30ff6576f504956c
csalaz5 <113476632+CSALAZ5@users.noreply.github.com>
2024-02-29 13:12:36 -0500

    Delete install-dependencies.sh

--- install-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash 
-set -eux 
-rm ~/.npmrc | true 
-curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc 
-npm ci || exit 1 
-git reset --hard || exit 1
